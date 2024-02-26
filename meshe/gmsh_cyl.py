c61a2e0622f05b5a2d1320bc29c69cbeee8aa7ce
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-02-26 13:36:29 +0000

    perform some corrections in cylinder meshing

--- meshe/gmsh_cyl.py
+++ meshe/gmsh_cyl.py
@@ -11,10 +11,14 @@ mesh_size = 0.25
 factory = gmsh.model.geo
 factory.addPoint(0, 0, 0,  tag = 1)
 factory.addPoint(0, radius, 0,  tag = 2)
-factory.addPoint(0, -radius, 0, tag = 3)
+factory.addPoint(-radius, 0, 0, tag = 3)
+factory.addPoint(0, -radius, 0, tag = 4)
+factory.addPoint(radius, 0, 0, tag = 5)
 circle_curve1 = factory.addCircleArc(2,1,3, tag = 1)
-circle_curve2 = factory.addCircleArc(3,1,2, tag = 2)
-circle = factory.addCurveLoop([1,2], tag = 3)
+circle_curve2 = factory.addCircleArc(3,1,4, tag = 2)
+circle_curve2 = factory.addCircleArc(4,1,5, tag = 3)
+circle_curve2 = factory.addCircleArc(5,1,2, tag = 4)
+circle = factory.addCurveLoop([1,2,3,4], tag = 3)
 print(circle)
 # Create a surface from the circle
 surface = factory.addPlaneSurface([circle],tag = 1)
@@ -22,14 +26,14 @@ print(surface)
 # Extrude the surface
 extruded_volume = factory.extrude([(2,surface)],0,0,height)
 print(extruded_volume)
+factory.synchronize()
 volume = gmsh.model.addPhysicalGroup(3, [extruded_volume[1][1]], name="fluid")
-surface1 = gmsh.model.addPhysicalGroup(2, [extruded_volume[0][1]], tag = 101, name="surface1")
-surface2 = gmsh.model.addPhysicalGroup(2, [extruded_volume[2][1]], tag = 102, name="surface2")
-surface3 = gmsh.model.addPhysicalGroup(2, [extruded_volume[3][1]], tag = 103, name="surface3")
+inlet = gmsh.model.addPhysicalGroup(2, [1], tag = 101, name="inlet")
+outlet = gmsh.model.addPhysicalGroup(2, [26], tag = 102, name="outlet")
+surface3 = gmsh.model.addPhysicalGroup(2, [13,17,21,25], tag = 103, name="wall")
 
 # Generate the mesh
 factory.synchronize()
-gmsh.model.mesh.setSize(extruded_volume, mesh_size)
-
-
 gmsh.model.mesh.generate(3)
+gmsh.write('test.msh')
+gmsh.finalize()
