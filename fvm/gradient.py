the least squares gradient computation now account for boudary values : done in order to fix gradients of elements that have many boundary faces

fvm/gradient.py
@@ -30,7 +30,7 @@ class ElementsGradientComputer :
 
 class LSGradient(ElementsGradientComputer): 
     
-    def __init__(self,dataname, gdataname, mesh, weighting = False): 
+    def __init__(self,dataname, gdataname, mesh, weighting = False, use_boundaries = False): 
         '''
         arguments 
         dataname ::: string ::: name of the data for which 
@@ -40,11 +40,13 @@ class LSGradient(ElementsGradientComputer):
         gradient would be computed 
         weighting ::: bool ::: wether or not use distance weights 
         for calculating gradients. 
+        use_boundaries ::: bool ::: 
         '''
         super().__init__(dataname, gdataname, mesh)
         self.weighting = weighting
+        self.use_boundaries = use_boundaries
 
-    def calc_element_gradient(self,elem_indice):
+    def calc_element_gradient(self,elem_indice ):
         '''
         When considering fixed mesh, this routine may be opptimized by precomputing 
         the element/neighbors distance, 
@@ -78,6 +80,22 @@ class LSGradient(ElementsGradientComputer):
             distance_matrix.append(distance)
             delta_data_vector.append(delta_data)
             weights_vector.append(weight)
+        if self.use_boundaries : 
+            el_bndface_conn = self.mesh.elements_bndf_conn[elem_indice]
+            for face_index in el_bndface_conn : 
+                neigh_bndf_indices = self.mesh.bndfaces[face_index]
+                neigh_bndf = self.mesh.nodes[neigh_bndf_indices]
+                # get neighbor centroid and data value
+                neigh_centroid = self.mesh._calc_centroid(neigh_bndf)
+                neigh_data = self.mesh.bndfaces_data[self.dataname][face_index]
+                # Update distance matrix and delta vector (delta of data)
+                distance = neigh_centroid - el_centroid
+                delta_data = neigh_data - el_data
+                absolute_distance = np.sqrt(np.sum(distance**2.))
+                weight = 1/absolute_distance
+                distance_matrix.append(distance)
+                delta_data_vector.append(delta_data)
+                weights_vector.append(weight)
         # Uses Pseudo inverse to solve the gradients
         delta_data_vector = np.asarray(delta_data_vector)
         distance_matrix = np.asarray(distance_matrix)

meshe/gmsh_cyl.py
@@ -47,6 +47,7 @@ mesh = TetraMesh()
 mesh.gmsh_reader('test.msh')
 mesh.set_internal_faces()
 mesh.set_elements_intfaces_connectivity()
+mesh.set_boundary_faces()
 print('NUmber of nodes : ', np.shape(mesh.nodes))
 print('Number of elements :',np.shape(mesh.elements))
 print('Number of boundary faces :',np.shape(mesh.bndfaces))
@@ -56,8 +57,9 @@ print('Number of internal faces :', np.shape(mesh.intfaces))
 def function(x,y,z):
     return 4*x + 0*y + 0*z
 mesh.set_elements_data('T', function)
+mesh.set_bndfaces_data('T', function)
 # calc data gradient 
-grad_computer = LSGradient('T','gradT', mesh,weighting=True)
+grad_computer = LSGradient('T','gradT', mesh,weighting=True,use_boundaries=True)
 #count = 0 
 #for i in range(len(mesh.elements)):
 #    grad = grad_computer.calc_element_gradient(i)
@@ -65,10 +67,11 @@ grad_computer = LSGradient('T','gradT', mesh,weighting=True)
 #    err = np.abs((4 - gradx)/4) * 100
 #    if err > 1 : 
 #        count += 1
-#        print(i,grad)
-#        print(mesh.elements_intf_conn[i]) 
+#    print(i,grad)
+#    print(mesh.elements_intf_conn[i]) 
 #print(count)
 grad_computer.calculate_gradients()
+print(mesh.elements_data['gradT'])
 # 
 mesh.set_boundary_faces()
 boundary_conditions = {'inlet' : {'type' : 'dirichlet',

meshe/mesh.py
@@ -26,25 +26,27 @@ class Mesh :
         self.nodes = None 
         # data 
         self.elements_centroids = None 
+        self.bndfaces_centroids = None 
         self.elements_data = {}
+        self.bndfaces_data = {}
         self.physical_entities = None
         
     def save_vtk(self,output_file = 'dump.vtk'):
         '''
         '''
         # Create meshio.Mesh object
-        print(self.elements_data)
+        #print(self.elements_data)
         data = {}
         for key,val in self.elements_data.items():
             data[key] = [val.tolist()]
-            print(key, np.shape(val))
+            #print(key, np.shape(val))
         #data = {'T' : [self.elements_data['orthodiff_solution']]}
         mesh = meshio.Mesh(
             points=self.nodes,
             cells=[("tetra", self.elements)],
             cell_data=data
         )
-        print(mesh.cell_data)
+        #print(mesh.cell_data)
         meshio.write(output_file, mesh, file_format="vtk")
         
     def _get_bc_index(self,bc_name): 
@@ -89,6 +91,32 @@ class Mesh :
         z_arr = self.elements_centroids[:,2]
         data_arr = functional(x_arr,y_arr,z_arr)
         self.elements_data[dataname] = data_arr
+    
+    def set_bndfaces_centroids(self):
+        '''
+        set the doundary faces centroid array 
+        '''
+        centroids_arr = np.zeros((np.size(self.bndfaces,0),3))
+        for i,face in enumerate(self.bndfaces) : 
+            face_nodes_coordinates = self.nodes[face]
+            centroid = self._calc_centroid(face_nodes_coordinates)
+            centroids_arr[i,:] = centroid
+        self.bndfaces_centroids = centroids_arr
+    
+    def set_bndfaces_data(self,dataname, functional):
+        '''
+        argument 
+        dataname ::: string ::: name of the element data 
+        functional ::: callable object ::: function of centroids coordinates 
+        that defines the data 
+        '''
+        if np.all(self.bndfaces_centroids == None) :
+            self.set_bndfaces_centroids()
+        x_arr = self.bndfaces_centroids[:,0]
+        y_arr = self.bndfaces_centroids[:,1]
+        z_arr = self.bndfaces_centroids[:,2]
+        data_arr = functional(x_arr,y_arr,z_arr)
+        self.bndfaces_data[dataname] = data_arr
 
     def set_internal_faces(self): 
         '''

tests/test_fvm_schemes.py
@@ -6,6 +6,8 @@ from meshe.mesh import TetraMesh
 from fvm.diffusion import * 
 from fvm.interpolation import * 
 
+EPSILON = 1e-10
+
 @pytest.fixture 
 def lsgrad_fixture():
     mesh = TetraMesh()
@@ -16,11 +18,20 @@ def lsgrad_fixture():
                            [-1, 0, 0]])
     mesh.elements = np.array([[0, 1, 2, 3],
                               [0,4,1,3]])
+    mesh.bndfaces = np.array([[0,1,2],
+                              [0,2,3],
+                              [1,2,3],
+                              [0,4,1],
+                              [0,4,3],
+                              [4,1,3]])
+    
     mesh.set_internal_faces()
     mesh.set_elements_intfaces_connectivity()
+    mesh.set_boundary_faces()
     def function(x,y,z):
         return 4*x + 0*y + 0*z
     mesh.set_elements_data('T', function)
+    mesh.set_bndfaces_data('T', function)
     grad_computer = LSGradient('T','gradT', mesh)
     return grad_computer
 
@@ -518,4 +529,14 @@ def test_nonortho_diff_operator2(mesh_fixture5):
         print(mesh_fixture5.elements_data['temp'])
         #print(mesh_fixture5.elements_data['grad_temp'])
     assertion = False 
-    assert assertion 
+    assert assertion
+    
+def test_ls_gradient_with_bndval(lsgrad_fixture): 
+    lsgrad_fixture.use_boundaries = True
+    gradient0 = lsgrad_fixture.calc_element_gradient(0)
+    gradient1 = lsgrad_fixture.calc_element_gradient(1)
+    expected_grad = np.array([4, 0, 0])
+    print(gradient0)
+    print(gradient1)
+    assertion = np.all(np.abs(gradient0 - expected_grad) < EPSILON) and np.all(np.abs(gradient1 - expected_grad) < EPSILON)
+    assert assertion  