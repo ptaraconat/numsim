aa87abf4601ad3c3778db0ff229963dd5aaec2d7
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-03-18 12:53:34 +0000

    add linear differencing convection computation, for internal faces

---
+++ fvm/convection.py
@@ -0,0 +1,44 @@
+import numpy as np 
+
+from .diffusion import FaceComputer
+from .interpolation import FaceInterpolattion
+
+class CentralDiffConvection(FaceComputer):
+
+    def __init__(self,velocity_data = ''):
+        '''
+        '''
+        self.velocity_data = velocity_data
+    
+    def calc_surface_coef(self, centroid1, centroid2, surface_area, surface_vector, face_vertex,velocity1, velocity2):
+        '''
+        arguments 
+        centroid1 ::: np.array(3,) ::: coordinates of first node
+        centroid2 ::: np.array(3,) ::: coordinates of the second node 
+        surface_area ::: float ::: Area of face associated with the pair of nodes 
+        centroid1/centroid2
+        surface_vector ::: np.array(3,) ::: vector associated with that face
+        face_vertex ::: np.array(3,) ::: Point that belong to the face. Typically, its centroid 
+        velocity1 ::: np.array(3,) ::: Convective velocity at centroid1
+        velocity2 ::: np.array(3,) ::: Convective velocity at centroid2
+        returns 
+        surf_flux ::: float ::: surface flux through the surface induced associated with 
+        the convection 
+        weight1 ::: float ::: 
+        weight2 ::: float ::: 
+        '''
+        face_velocity = FaceInterpolattion().face_computation(centroid1, centroid2, 
+                                                              velocity1, velocity2, 
+                                                              face_vertex)
+        #
+        # Calculate distances between centroids and pair_nodes/face intersection 
+        distance1 = np.sqrt(np.sum( (face_vertex-centroid1)**2. ))
+        distance2 = np.sqrt(np.sum( (face_vertex-centroid2)**2. ))
+        # Calculate distances between centroid sharing the face 
+        centroids_distance = distance1 + distance2 #np.sqrt(np.sum( (centroid1-centroid2)**2. ))
+        # Calculate centroids weights 
+        weight1 = distance2/centroids_distance
+        weight2 = distance1/centroids_distance
+        #
+        surf_coeff = surface_area*np.abs(np.dot(surface_vector,face_velocity))
+        return surf_coeff, weight1, weight2
---
+++ tests/test_fvm_convection.py
@@ -0,0 +1,55 @@
+import pytest
+import sys as sys 
+sys.path.append('.')
+from fvm.convection import * 
+from meshe.mesh import TetraMesh
+
+@pytest.fixture 
+def mesh_fixture():
+    mesh = TetraMesh()
+    mesh.nodes = np.array([[0, 0, 0],
+                           [0, 1, 0],
+                           [1, 0, 0],
+                           [0, 0, 1],
+                           [0, 0, -1]])
+    mesh.elements = np.array([[0, 1, 2, 3],
+                              [0,4,1,3]])
+    mesh.set_internal_faces()
+    mesh.set_elements_intfaces_connectivity()
+    velocity_arr = np.array([[]])
+    mesh.elements_data['velocity'] = velocity_arr
+    return mesh 
+
+def test_conv_central_differencing():
+    convection_operator = CentralDiffConvection()
+    centroid1 = np.array([0,0,-1])
+    centroid2 = np.array([0,0,1])
+    surface_centroid = np.array([0,0,0])
+    surface_area = 0.5
+    surface_vector = np.array([0,0,1])
+    velocity1 = np.array([0,0,2])
+    velocity2 = np.array([0,0,1])
+    face_coeff, w1, w2 = convection_operator.calc_surface_coef(centroid1, centroid2, 
+                                                               surface_area, surface_vector, 
+                                                               surface_centroid,
+                                                               velocity1, velocity2)
+    print(face_coeff,w1,w2)
+    assertion = face_coeff == 0.75 and w1 == 0.5 and w2 == 0.5 
+    assert assertion 
+    
+def test_conv_central_differencing2():
+    convection_operator = CentralDiffConvection()
+    centroid1 = np.array([0,0,-1])
+    centroid2 = np.array([0,0,3])
+    surface_centroid = np.array([0,0,0])
+    surface_area = 0.5
+    surface_vector = np.array([0,0,1])
+    velocity1 = np.array([0,0,4])
+    velocity2 = np.array([0,0,1])
+    face_coeff, w1, w2 = convection_operator.calc_surface_coef(centroid1, centroid2, 
+                                                               surface_area, surface_vector, 
+                                                               surface_centroid,
+                                                               velocity1, velocity2)
+    print(face_coeff,w1,w2)
+    assertion = face_coeff == 1.625 and w1 == 0.75 and w2 == 0.25 
+    assert assertion 
