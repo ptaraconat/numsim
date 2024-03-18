end devellopement of non orthogonal diffusion. There may have bug on tetra mesh, induced by errors in gradients computations

fvm/gradient.py
@@ -88,14 +88,15 @@ class LSGradient(ElementsGradientComputer):
                 # get neighbor centroid and data value
                 neigh_centroid = self.mesh._calc_centroid(neigh_bndf)
                 neigh_data = self.mesh.bndfaces_data[self.dataname][face_index]
-                # Update distance matrix and delta vector (delta of data)
-                distance = neigh_centroid - el_centroid
-                delta_data = neigh_data - el_data
-                absolute_distance = np.sqrt(np.sum(distance**2.))
-                weight = 1/absolute_distance
-                distance_matrix.append(distance)
-                delta_data_vector.append(delta_data)
-                weights_vector.append(weight)
+                if neigh_data != None : 
+                    # Update distance matrix and delta vector (delta of data)
+                    distance = neigh_centroid - el_centroid
+                    delta_data = neigh_data - el_data
+                    absolute_distance = np.sqrt(np.sum(distance**2.))
+                    weight = 1/absolute_distance
+                    distance_matrix.append(distance)
+                    delta_data_vector.append(delta_data)
+                    weights_vector.append(weight)
         # Uses Pseudo inverse to solve the gradients
         delta_data_vector = np.asarray(delta_data_vector)
         distance_matrix = np.asarray(distance_matrix)

meshe/gmsh_cyl.py
@@ -86,18 +86,17 @@ mat, rhs_vec = diffusion_op(mesh,boundary_conditions)
 solution1 = np.linalg.solve(mat,rhs_vec)
 #print(solution)
 mesh.elements_data['orthodiff_solution'] = solution1
-mesh.save_vtk()
 
 # Init data 
 def function(x,y,z):
     return 0*x + 0*y + 0*z
 mesh.set_elements_data('temp', function)
 mesh.set_elements_data('grad_temp', function)
-gradient_computer = LSGradient('temp', 'grad_temp', mesh, weighting = False)
+gradient_computer = LSGradient('temp', 'grad_temp', mesh, weighting = False, use_boundaries = True)
 diff_op = NonOthogonalDiffusion(data_name = 'temp', 
                                 grad_data_name = 'grad_temp',
                                 method = 'over_relaxed')
-for i in range(1): 
+for i in range(10): 
         mat, rhs = diff_op(mesh, 
                            boundary_conditions, 
                            diffusion_coeff=1.)
@@ -105,5 +104,11 @@ for i in range(1):
         #print(rhs)
         solution = np.linalg.solve(mat,rhs)
         mesh.elements_data['temp'] = solution
+        mesh.set_bndfaces_data_from_bc('temp', boundary_conditions)
         gradient_computer.calculate_gradients()
-#print(np.abs(mesh.elements_data['temp']-solution1))
+        
+
+print(solution)
+print(np.mean(np.abs(mesh.elements_data['temp']-solution1)))
+
+mesh.save_vtk()

meshe/mesh.py
@@ -117,7 +117,29 @@ class Mesh :
         z_arr = self.bndfaces_centroids[:,2]
         data_arr = functional(x_arr,y_arr,z_arr)
         self.bndfaces_data[dataname] = data_arr
-
+        
+    def set_bndfaces_data_from_bc(self,dataname,dict):
+        '''
+        arguments 
+        dataname ::: string ::: name of the element data
+        dict ::: dictionnary ::: dictionary of uniform boundary condition
+        '''
+        # Init bndface_data with None 
+        self.bndfaces_data[dataname] = np.asarray([None for _ in range(np.size(self.bndfaces,0))])
+        # Loop over different boundary conditions 
+        for bc_key,val in dict.items():
+            bc_index = self._get_bc_index(bc_key)
+            type = val['type']
+            bc_val = val['value']
+            # get index associated with the current bondary condition 
+            surfaces_indices =np.squeeze(np.argwhere(self.bndfaces_tags == bc_index))
+            if surfaces_indices.shape == () : 
+                surfaces_indices = [surfaces_indices]
+            # Loop over the later boundary surfaces 
+            if type == 'dirichlet':
+                for i in surfaces_indices :
+                    self.bndfaces_data[dataname][i] = bc_val
+ 
     def set_internal_faces(self): 
         '''
         Set the internal faces tables and internal faces to element connectivity table 