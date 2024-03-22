f10f19d04a55fae3a0aa38124f1cde61d2b0c546
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-03-22 12:26:52 +0000

    continue devellopement of convection operator

--- fvm/convection.py
+++ fvm/convection.py
@@ -5,10 +5,11 @@ from .interpolation import FaceInterpolattion
 
 class CentralDiffConvection(FaceComputer):
 
-    def __init__(self,velocity_data = ''):
+    def __init__(self,velocity_data = '',convected_data = ''):
         '''
         '''
         self.velocity_data = velocity_data
+        self.convected_data = convected_data
     
     def calc_surface_coef(self, centroid1, centroid2, surface_area, surface_vector, face_vertex,velocity1, velocity2):
         '''
@@ -40,10 +41,14 @@ class CentralDiffConvection(FaceComputer):
         weight1 = distance2/centroids_distance
         weight2 = distance1/centroids_distance
         #
-        surf_coeff = surface_area*np.abs(np.dot(surface_vector,face_velocity))
+        sign = np.sign(np.dot(surface_vector,centroid2-centroid1))
+        if sign < 0 : 
+            surface_vector = - surface_vector
+        #
+        surf_coeff = surface_area*np.dot(surface_vector,face_velocity)
         return surf_coeff, weight1, weight2
     
-    def calc_dirichlet_surface_coeff(self, surface_area, surface_vector, surface_velocity):
+    def calc_dirichlet_surface_coeff(self, surface_area, surface_vector, surface_velocity,centroid,surface_centroid):
         '''
         arguments : 
         surface_area ::: float ::: Area of face associated with the pair of nodes 
@@ -51,10 +56,15 @@ class CentralDiffConvection(FaceComputer):
         surface_vector ::: np.array(3,) ::: vector associated with that face
         surface_velocity ::: np.array(3,) ::: Convective velocity at
         the boundary face
+        centroid ::: 
+        surface_centroid :::
         returns 
         surf_coeff ::: float ::: convective coefficient associated with the boundary surface 
         '''
-        surf_coeff = surface_area*np.abs(np.dot(surface_vector,surface_velocity))
+        sign = np.sign(np.dot(surface_vector,surface_centroid-centroid))
+        if sign < 0 : 
+            surface_vector = - surface_vector
+        surf_coeff = surface_area*np.dot(surface_vector,surface_velocity)
         return surf_coeff 
     
     def calc_neumann_surface_coeff(self,centroid, surface_centroid,surface_area, surface_vector, surface_velocity, centroid_value, surface_gradient):
@@ -73,10 +83,104 @@ class CentralDiffConvection(FaceComputer):
         surf_coeff ::: float ::: convective coefficient associated with the boundary surface
         surface_value ::: float ::: data value at the boundary face  
         '''
+        sign = np.sign(np.dot(surface_vector,surface_centroid-centroid))
+        if sign < 0 : 
+            surface_vector = - surface_vector
         centroids_distance = centroid - surface_centroid
         surface_value = centroid_value - np.dot(surface_gradient,centroids_distance)
+        rhs_comp =  np.dot(surface_gradient,centroids_distance)
         surf_coeff = surface_area*np.abs(np.dot(surface_vector,surface_velocity))
-        return surf_coeff, surface_value
+        return surf_coeff, rhs_comp
+    
+    def __call__(self,mesh, boundary_conditions):
+        '''
+        arguments 
+        mesh ::: numsim.meshe.mesh.Mesh ::: mesh on which the diffusion operator is calculated
+        boundary_conditions ::: dictionnary ::: dictionnary that specifies the boundary conditions 
+        returns 
+        matrix ::: np.array (n_elem,n_elem) :::
+        '''
+        n_elem = np.size(mesh.elements,0)
+        matrix = np.zeros((n_elem,n_elem))
+        rhs_vec = np.zeros((n_elem,1))
+        for ind_face,face in enumerate(mesh.intfaces) : 
+            ind_cent1 = mesh.intfaces_elem_conn[ind_face][0]
+            ind_cent2 = mesh.intfaces_elem_conn[ind_face][1]
+            centroid1 = mesh.elements_centroids[ind_cent1]
+            centroid2 = mesh.elements_centroids[ind_cent2]
+            #
+            coord_face = mesh.nodes[face]
+            face_area = mesh._calc_surface_area(coord_face)
+            face_normal = mesh._calc_surface_normal(coord_face)
+            face_centroid = mesh._calc_centroid(coord_face)
+            #
+            velocity1 = mesh.elements_data[self.velocity_data][ind_cent1]
+            velocity2 = mesh.elements_data[self.velocity_data][ind_cent2]
+            #
+            surf_flux, w1, w2 = self.calc_surface_coef(centroid1, centroid2, 
+                                                       face_area, 
+                                                       face_normal, 
+                                                       face_centroid,
+                                                       velocity1, 
+                                                       velocity2)
+            # Fill Matrix 
+            #
+            matrix[ind_cent1,ind_cent1] += w1*surf_flux
+            matrix[ind_cent1,ind_cent2] += w2*surf_flux
+            #
+            matrix[ind_cent2,ind_cent2] += -w2*surf_flux
+            matrix[ind_cent2,ind_cent1] += -w1*surf_flux
+            #
+            del centroid1, centroid2, velocity1, velocity2, w1, w2, ind_cent1, ind_cent2
+            del coord_face, face_area, face_normal, face_centroid, surf_flux
+        # Treat boundaries 
+        # Loop over different boundary conditions 
+        for bc_key,val in boundary_conditions.items():
+            bc_index = mesh._get_bc_index(bc_key)
+            type = val['type']
+            bc_val = val['value']
+            # get index associated with the current bondary condition 
+            surfaces_indices =np.squeeze(np.argwhere(mesh.bndfaces_tags == bc_index))
+            if surfaces_indices.shape == () : 
+                surfaces_indices = [surfaces_indices]
+            # Loop over the later boundary surfaces 
+            for i in surfaces_indices :
+                # get face nodes 
+                bnd_face = mesh.bndfaces[i]
+                elem_ind = int(mesh.bndfaces_elem_conn[i][0])
+                face_nodes = mesh.nodes[bnd_face]
+                if type == 'dirichlet':
+                    # Treat a dirichlet bc
+                    centroid = mesh.elements_centroids[elem_ind]
+                    face_centroid = mesh._calc_centroid(face_nodes)
+                    surface_area = mesh._calc_surface_area(face_nodes) 
+                    surface_normal = mesh._calc_surface_normal(face_nodes)
+                    surface_velocity = mesh.bndfaces_data[self.velocity_data][i]
+                    face_coeff = self.calc_dirichlet_surface_coeff(surface_area, surface_normal, surface_velocity, centroid, face_centroid)
+                    bc_dir_value = bc_val
+                    # rework ::: check sign
+                    rhs_vec[elem_ind] += -bc_dir_value*face_coeff 
+                    del surface_area, surface_normal, surface_velocity, face_coeff, bc_dir_value
+                if type == 'neumann' : 
+                    # Treat a Von Neumann bc 
+                    centroid = mesh.elements_centroids[elem_ind]
+                    surface_centroid = mesh._calc_centroid(face_nodes)
+                    surface_area = mesh._calc_surface_area(face_nodes)
+                    surface_normal = mesh._calc_surface_normal(face_nodes)
+                    surface_velocity =mesh.bndfaces_data[self.velocity_data][i]
+                    centroid_value = mesh.elements_data[self.convected_data][elem_ind]
+                    bc_neu_value =  bc_val
+                    face_coeff, face_value = self.calc_neumann_surface_coeff(centroid, 
+                                                                 surface_centroid,
+                                                                 surface_area, 
+                                                                 surface_normal, 
+                                                                 surface_velocity, 
+                                                                 centroid_value, 
+                                                                 bc_neu_value)
+                    # rework ::: check sign
+                    matrix[elem_ind,elem_ind] += face_coeff
+                    rhs_vec[elem_ind] += face_coeff*face_value
+        return matrix, rhs_vec
     
 class UpwindConvection(FaceComputer): 
     
--- tests/test_fvm_convection.py
+++ tests/test_fvm_convection.py
@@ -2,7 +2,8 @@ import pytest
 import sys as sys 
 sys.path.append('.')
 from fvm.convection import * 
-from meshe.mesh import TetraMesh
+from fvm.diffusion import * 
+from meshe.mesh import Mesh, TetraMesh
 
 @pytest.fixture 
 def mesh_fixture():
@@ -19,6 +20,72 @@ def mesh_fixture():
     velocity_arr = np.array([[]])
     mesh.elements_data['velocity'] = velocity_arr
     return mesh 
+
+@pytest.fixture 
+def mesh_fixture4():
+    mesh = Mesh()
+    dx = 1. 
+    mesh.nodes = np.array([[0, 0 ,0],
+                           [0, dx, 0],
+                           [0, dx, dx],
+                           [0, 0, dx],
+                           [dx, 0 ,0],
+                           [dx, dx, 0],
+                           [dx, dx, dx],
+                           [dx, 0, dx],
+                           [2*dx, 0 ,0],
+                           [2*dx, dx, 0],
+                           [2*dx, dx, dx],
+                           [2*dx, 0, dx],
+                           [3*dx, 0 ,0],
+                           [3*dx, dx, 0],
+                           [3*dx, dx, dx],
+                           [3*dx, 0, dx]])
+    mesh.elements = np.array([[1,2,3,4,5,6,7,8],
+                              [5,6,7,8,9,10,11,12],
+                              [9,10,11,12,13,14,15,16]]) - 1
+    mesh.bndfaces = np.array([[1,2,3,4],
+                              [1,5,8,4],
+                              [2,6,7,3],
+                              [5,9,12,8],
+                              [6,10,11,7],
+                              [9,13,16,12],
+                              [10,14,15,11],
+                              [13,14,15,16]]) -1
+    mesh.intfaces = np.array([[5,6,7,8],
+                              [9,10,11,12]]) -1
+    mesh.intfaces_elem_conn = np.array([[1,2],
+                                        [2,3]]) - 1
+    mesh.elements_bndf_conn = [[0,1,2],
+                               [3,4],
+                               [5,6,7]]
+    mesh.bndfaces_elem_conn = np.expand_dims(np.array([1,1,1,2,2,3,3,3]),axis = 1)-1
+    mesh.bndfaces_tags = np.array([1,2,2,2,2,2,2,3])
+    mesh.physical_entities = {'inlet': np.array([1,   2]), 
+                              'outlet': np.array([3,   2]), 
+                              'wall': np.array([2,   2])}
+
+    mesh.set_elements_intfaces_connectivity()
+    mesh.set_elements_centroids()
+    #mesh.set_boundary_faces()
+    # set data 
+    arr_tmp = np.array([[1,0,0],
+                         [1,0,0],
+                         [1,0,0]])
+    velocity = 4
+    mesh.elements_data['velocity'] = velocity * arr_tmp
+    mesh.elements_data['temp'] = np.zeros((3,1))
+    arr_tmp = np.array([[1,0,0],
+                         [1,0,0],
+                         [1,0,0],
+                         [1,0,0],
+                         [1,0,0],
+                         [1,0,0],
+                         [1,0,0],
+                         [1,0,0]])
+    mesh.bndfaces_data['velocity'] =   velocity * arr_tmp 
+    
+    return mesh
 
 def test_conv_central_differencing():
     convection_operator = CentralDiffConvection()
@@ -111,4 +178,45 @@ def test_conv_neum_central_diff():
     
     print(face_coeff, face_value)
     assertion = face_coeff == 1. and face_value == 3
+    assert assertion 
+    
+def test_central_diff_operator(mesh_fixture4):
+    operator = CentralDiffConvection(velocity_data= 'velocity',convected_data = 'temp')
+    boundary_conditions = {'inlet' : {'type' : 'dirichlet',
+                                      'value' : 3},
+                           'outlet' : {'type' : 'dirichlet',
+                                       'value' : 0},
+                           'wall' : {'type' : 'neumann',
+                                     'value' : np.array([0,0,0])}}
+    print(mesh_fixture4.bndfaces)
+    mat, rhs = operator(mesh_fixture4,boundary_conditions)
+    print(mat)
+    print(rhs)
+    solution = np.dot(np.linalg.pinv(mat),rhs)
+    print(solution)
+    assertion = False 
+    assert assertion 
+    
+def test_central_diff_operator__(mesh_fixture4):
+    operator = CentralDiffConvection(velocity_data= 'velocity',convected_data = 'temp')
+    diff_op = OrthogonalDiffusion()
+    boundary_conditions = {'inlet' : {'type' : 'dirichlet',
+                                      'value' : 3},
+                           'outlet' : {'type' : 'dirichlet',
+                                       'value' : 0},
+                           'wall' : {'type' : 'neumann',
+                                     'value' : np.array([0,0,0])}}
+    mat, rhs = operator(mesh_fixture4,boundary_conditions)
+    mat_, rhs_ = diff_op(mesh_fixture4, 
+                       boundary_conditions, 
+                       diffusion_coeff=1.)
+    print(mat)
+    print(mat_)
+    mat += mat_
+    rhs += rhs_
+    print(mat)
+    print(rhs)
+    solution = np.dot(np.linalg.pinv(mat),rhs)
+    print(solution)
+    assertion = False 
     assert assertion 
