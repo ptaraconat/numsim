ce178ac45d59c2d39935e028138810950e8f1561
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-03-27 12:00:18 +0000

    rearange convection operator classes : boundary treatements are commun to centraldiff and upwind schemes

--- fvm/convection.py
+++ fvm/convection.py
@@ -3,50 +3,13 @@ import numpy as np
 from .diffusion import FaceComputer
 from .interpolation import FaceInterpolattion
 
-class CentralDiffConvection(FaceComputer):
 
-    def __init__(self,velocity_data = '',convected_data = ''):
+class ConvectionOperator(FaceComputer):
+    def __init__(self,velocity_data = '', convected_data = ''):
         '''
         '''
         self.velocity_data = velocity_data
         self.convected_data = convected_data
-    
-    def calc_surface_coef(self, centroid1, centroid2, surface_area, surface_vector, face_vertex,velocity1, velocity2):
-        '''
-        arguments 
-        centroid1 ::: np.array(3,) ::: coordinates of first node
-        centroid2 ::: np.array(3,) ::: coordinates of the second node 
-        surface_area ::: float ::: Area of face associated with the pair of nodes 
-        centroid1/centroid2
-        surface_vector ::: np.array(3,) ::: vector associated with that face
-        face_vertex ::: np.array(3,) ::: Point that belong to the face. Typically, its centroid 
-        velocity1 ::: np.array(3,) ::: Convective velocity at centroid1
-        velocity2 ::: np.array(3,) ::: Convective velocity at centroid2
-        returns 
-        surf_flux ::: float ::: surface flux through the surface induced associated with 
-        the convection 
-        weight1 ::: float ::: 
-        weight2 ::: float ::: 
-        '''
-        face_velocity = FaceInterpolattion().face_computation(centroid1, centroid2, 
-                                                              velocity1, velocity2, 
-                                                              face_vertex)
-        #
-        # Calculate distances between centroids and pair_nodes/face intersection 
-        distance1 = np.sqrt(np.sum( (face_vertex-centroid1)**2. ))
-        distance2 = np.sqrt(np.sum( (face_vertex-centroid2)**2. ))
-        # Calculate distances between centroid sharing the face 
-        centroids_distance = distance1 + distance2 #np.sqrt(np.sum( (centroid1-centroid2)**2. ))
-        # Calculate centroids weights 
-        weight1 = distance2/centroids_distance
-        weight2 = distance1/centroids_distance
-        #
-        sign = np.sign(np.dot(surface_vector,centroid2-centroid1))
-        if sign < 0 : 
-            surface_vector = - surface_vector
-        #
-        surf_coeff = surface_area*np.dot(surface_vector,face_velocity)
-        return surf_coeff, weight1, weight2
     
     def calc_dirichlet_surface_coeff(self, surface_area, surface_vector, surface_velocity,centroid,surface_centroid):
         '''
@@ -180,13 +143,57 @@ class CentralDiffConvection(FaceComputer):
                     matrix[elem_ind,elem_ind] += -face_coeff
                     rhs_vec[elem_ind] += -face_coeff*face_value
         return matrix, rhs_vec
-    
-class UpwindConvection(FaceComputer): 
-    
-    def __init__(self,velocity_data = ''):
+
+class CentralDiffConvection(ConvectionOperator):
+
+    def __init__(self,velocity_data = '',convected_data = ''):
         '''
         '''
-        self.velocity_data = velocity_data 
+        super().__init__(velocity_data=velocity_data,convected_data=convected_data)
+
+    def calc_surface_coef(self, centroid1, centroid2, surface_area, surface_vector, face_vertex,velocity1, velocity2):
+        '''
+        arguments 
+        centroid1 ::: np.array(3,) ::: coordinates of first node
+        centroid2 ::: np.array(3,) ::: coordinates of the second node 
+        surface_area ::: float ::: Area of face associated with the pair of nodes 
+        centroid1/centroid2
+        surface_vector ::: np.array(3,) ::: vector associated with that face
+        face_vertex ::: np.array(3,) ::: Point that belong to the face. Typically, its centroid 
+        velocity1 ::: np.array(3,) ::: Convective velocity at centroid1
+        velocity2 ::: np.array(3,) ::: Convective velocity at centroid2
+        returns 
+        surf_flux ::: float ::: surface flux through the surface induced associated with 
+        the convection 
+        weight1 ::: float ::: 
+        weight2 ::: float ::: 
+        '''
+        face_velocity = FaceInterpolattion().face_computation(centroid1, centroid2, 
+                                                              velocity1, velocity2, 
+                                                              face_vertex)
+        #
+        # Calculate distances between centroids and pair_nodes/face intersection 
+        distance1 = np.sqrt(np.sum( (face_vertex-centroid1)**2. ))
+        distance2 = np.sqrt(np.sum( (face_vertex-centroid2)**2. ))
+        # Calculate distances between centroid sharing the face 
+        centroids_distance = distance1 + distance2 #np.sqrt(np.sum( (centroid1-centroid2)**2. ))
+        # Calculate centroids weights 
+        weight1 = distance2/centroids_distance
+        weight2 = distance1/centroids_distance
+        #
+        sign = np.sign(np.dot(surface_vector,centroid2-centroid1))
+        if sign < 0 : 
+            surface_vector = - surface_vector
+        #
+        surf_coeff = surface_area*np.dot(surface_vector,face_velocity)
+        return surf_coeff, weight1, weight2
+  
+class UpwindConvection(ConvectionOperator): 
+    
+    def __init__(self,velocity_data = '', convected_data = ''):
+        '''
+        '''
+        super().__init__(velocity_data=velocity_data,convected_data=convected_data)
     
     def calc_surface_coef(self,centroid1, centroid2, surface_area, surface_vector, face_vertex, velocity1, velocity2):
         '''
