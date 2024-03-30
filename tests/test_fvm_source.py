fix issue in the sign of source term

tests/test_fvm_source.py
@@ -39,6 +39,6 @@ def test_source_term(mesh_fixture):
 def test_source_operator(mesh_fixture2): 
     source_operator = SourceTerm(data_name = 'source')
     rhs_vec = source_operator(mesh_fixture2)
-    expected_rhs = np.array([[0],[0],[94.5],[0],[0]])
+    expected_rhs = np.array([[0],[0],[-94.5],[0],[0]])
     assertion = np.all(rhs_vec == expected_rhs)
     assert assertion 

tutorials/1D_convdiff_withsource.py
@@ -6,8 +6,9 @@ from fvm.diffusion import OrthogonalDiffusion
 from fvm.source_term import SourceTerm
 from meshe.mesh import Mesh1D
 
-velocity = 0.2
-diffusion_coeff = 1.
+velocity = 0.2#0.2
+diffusion_coeff = 0.5#1.
+source_val = 1
 
 dx = 1. 
 n_elem = 20
@@ -27,11 +28,11 @@ arr_tmp = np.zeros((n_bndf,3))
 arr_tmp[:,0] = 1. 
 mesh.bndfaces_data['velocity'] =   velocity * arr_tmp 
 arr_tmp = np.zeros((n_elem,))
-arr_tmp[source_loc] = 3.5
+arr_tmp[source_loc] = source_val
 mesh.elements_data['source'] = arr_tmp
 
 boundary_conditions = {'inlet' : {'type' : 'dirichlet',
-                                  'value' : 3},
+                                  'value' : 0},
                        'outlet' : {'type' : 'dirichlet',
                                    'value' : 0},
                        'wall' : {'type' : 'neumann',
@@ -47,7 +48,7 @@ mat_c, rhs_c = diff_op(mesh,
                      diffusion_coeff=diffusion_coeff)
 rhs_source = source_op(mesh)
 mat = mat_d + mat_c
-rhs = rhs_d + rhs_c - rhs_source
+rhs = rhs_d + rhs_c + rhs_source
 solution = np.dot(np.linalg.pinv(mat),rhs)
 
 print(solution)