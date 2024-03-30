fix bug in computation of source element for a given element

fvm/source_term.py
@@ -8,13 +8,14 @@ class SourceTerm():
         '''
         self.data_name = data_name
     
-    def calc_element_coeff(self,element,source_value):
+    def calc_element_coeff(self,element_faces,element_centroid,source_value):
         '''
-        element ::: np.array(n_nodes,3) ::: coordinates of element nodes 
+        element_faces ::: list of np.array(n_nodes,3) ::: list of 
+        arrays of faces coordinates dounding a given element
+        element_centroid ::: np.array(3,) ::: centroid of the element
         source_value ::: float ::: source term at element centroid 
         returns 
         element_coeff ::: float ::: 
         '''
-        centroid = Mesh()._calc_centroid(element)
-        element_volume = Mesh()._calc_element_volume(element,centroid)
+        element_volume = Mesh()._calc_element_volume(element_faces,element_centroid)
         return element_volume*source_value 

meshe/mesh.py
@@ -353,7 +353,7 @@ class Mesh :
         '''
         #centroid = self._calc_centroid(element)
         volume = 0 
-        for element_surface in element_surfaces : 
+        for element_surface in element_surfaces :
             volflux = self._calc_surface_volflux(element_surface,centroid)
             volume = volume + volflux
         return volume
@@ -442,6 +442,7 @@ class Mesh1D(Mesh):
         bndfaces_list += outlet_bndface
         bndfaces_tags_list += [3]
         bndfaces_elem_conn_list += [elem_id]
+        elements_bndf_conn_list[-1].append(len(bndfaces_list)-1)
         #
         self.nodes = np.asarray(nodes_list)
         self.elements = np.asarray(elements_list)

tests/test_fvm_source.py
@@ -10,27 +10,26 @@ def mesh_fixture():
     return mesh
 
 def test_source_term(mesh_fixture):
-    print(mesh_fixture.elements)
-    print(mesh_fixture.elements_bndf_conn)
-    print(mesh_fixture.elements_intf_conn)
     el_ind = 0
+    #
     intf_ind = mesh_fixture.elements_intf_conn[el_ind]
     intfaces = mesh_fixture.intfaces[intf_ind]
     bndf_ind = mesh_fixture.elements_bndf_conn[el_ind]
     bndfaces = mesh_fixture.bndfaces[bndf_ind]
     element_faces_ind = np.concatenate((intfaces , bndfaces),axis = 0)
-    print(bndfaces)
-    print(intfaces)
-    print(element_faces_ind)
-    element_surfaces = [ ]
+    element_surfaces = []
     for i in range(np.size(element_faces_ind,0)) : 
         ind = element_faces_ind[i,:]
-        print(ind)
-        element_surfaces.append(mesh_fixture.nodes[ind])
-    
+        face_tmp = mesh_fixture.nodes[ind]
+        element_surfaces.append(face_tmp)
+    #
     source_value = 3.5
+    element = mesh_fixture.nodes[mesh_fixture.elements[el_ind]]
+    element_centroid = mesh_fixture._calc_centroid(element)
     source_operator = SourceTerm()
-    source_coeff = source_operator.calc_element_coeff(element_surfaces,source_value)
+    source_coeff = source_operator.calc_element_coeff(element_surfaces,
+                                                      element_centroid,
+                                                      source_value)
     print(source_coeff)
-    assertion = False 
+    assertion = source_coeff == 94.5
     assert assertion 