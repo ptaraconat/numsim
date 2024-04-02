78ad626f9f2893ba55a931295965eadf2f4e33c9
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-04-02 11:16:58 +0000

    add element_volumes calculation routine and start the devellopement of Euler Time scheme

---
+++ tstep/fdts.py
@@ -0,0 +1,61 @@
+import numpy as np 
+
+class TimeSteping : 
+    
+    def __init__(self):
+        '''
+        '''
+        self.dt = None
+        
+    def set_timestep(self,time_step) : 
+        '''
+        argument 
+        time_step ::: float ::: time step 
+        '''
+        self.dt = time_step
+        
+    def _calc_dt_conv(self,cfl_number,velocity_array, meshsize_array):
+        '''
+        arguments 
+        cfl_number ::: float ::: Courant number
+        velocity_arra ::: np.array(n_elem,3) ::: velocities array
+        meshsize_array ::: np.array(n_elem,1) ::: array of elements size 
+        returns 
+        dt ::: float ::: time step 
+        '''
+        pass
+    
+    def _calc_dt_diff(self,fourier_number,diffusivity_array, meshsize_array):
+        '''
+        arguments 
+        fourier_number ::: float ::: fourier number
+        diffusivity_array ::: np.array(n_elem,1) ::: diffusivities array
+        meshsize_array ::: np.array(n_elem,1) ::: array of elements size 
+        returns 
+        dt ::: float ::: time step 
+        '''
+        pass
+    
+class EulerScheme(TimeSteping):
+    
+    def __init__(self):
+        '''
+        '''
+        super().__init__()
+    
+    def step(self,current_array, mesh, implicit_contribution, explicit_contribution):
+        '''
+        arguments 
+        current_array ::: np.array(n_elem,1) ::: 
+        mesh ::: numsim.meshe.mesh.Mesh :::
+        implicit_contribution ::: np.array(n_elem,n_elem) :::
+        explicit_contribution ::: np.array(n_elem,1) :::
+        returns 
+        advanced_array ::: np.array(n_elem,1) ::: 
+        '''
+        # f_n+1 = f_n + (dt/V)*(IMP*f_n + EXP)
+        one_over_vol = 1. / mesh.elements_volumes
+        implicit_contrib = np.dot(implicit_contribution,current_array)
+        explicit_contrib = explicit_contribution
+        advanced_arry = current_array + self.dt*one_over_vol *(implicit_contrib + explicit_contrib)
+    
