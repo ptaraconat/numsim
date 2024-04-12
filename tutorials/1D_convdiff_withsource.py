991d4c6f941a3652b972796a382a7a4cee83a9ea
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2024-04-12 18:43:43 +0000

    fix 1D tutorials

--- tutorials/1D_convdiff_withsource.py
+++ tutorials/1D_convdiff_withsource.py
@@ -19,17 +19,24 @@ mesh.physical_entities = {'inlet': np.array([1,   2]),
                           'outlet': np.array([3,   2]), 
                           'wall': np.array([2,   2])}
 # set data 
+n_bndf = np.size(mesh.bndfaces,0)
+#
 arr_tmp = np.zeros((n_elem,3))
 arr_tmp[:,0] = 1. 
 mesh.elements_data['velocity'] = velocity * arr_tmp
 mesh.elements_data['temp'] = np.zeros((n_elem,1))
-n_bndf = np.size(mesh.bndfaces,0)
 arr_tmp = np.zeros((n_bndf,3))
 arr_tmp[:,0] = 1. 
 mesh.bndfaces_data['velocity'] =   velocity * arr_tmp 
+#
 arr_tmp = np.zeros((n_elem,))
 arr_tmp[source_loc] = source_val
 mesh.elements_data['source'] = arr_tmp
+#
+arr_tmp = np.ones((n_elem,1))
+mesh.elements_data['diffusion'] = diffusion_coeff * arr_tmp
+arr_tmp = np.ones((n_bndf,1))
+mesh.bndfaces_data['diffusion'] = diffusion_coeff * arr_tmp
 
 boundary_conditions = {'inlet' : {'type' : 'dirichlet',
                                   'value' : 0},
@@ -38,14 +45,13 @@ boundary_conditions = {'inlet' : {'type' : 'dirichlet',
                        'wall' : {'type' : 'neumann',
                                  'value' : np.array([0,0,0])}}
 
-operator = CentralDiffConvection(velocity_data= 'velocity',convected_data = 'temp')
-diff_op = OrthogonalDiffusion()
+operator = CentralDiffConvection(velocity_data= 'velocity')
+diff_op = OrthogonalDiffusion(diffusion_data= 'diffusion')
 source_op = SourceTerm(data_name = 'source')
 
 mat_d, rhs_d = operator(mesh,boundary_conditions)
 mat_c, rhs_c = diff_op(mesh, 
-                     boundary_conditions, 
-                     diffusion_coeff=diffusion_coeff)
+                     boundary_conditions)
 rhs_source = source_op(mesh)
 mat = mat_d + mat_c
 rhs = rhs_d + rhs_c + rhs_source
--- tutorials/1D_transient_convdiff.py
+++ tutorials/1D_transient_convdiff.py
@@ -21,14 +21,20 @@ mesh.physical_entities = {'inlet': np.array([1,   2]),
                           'wall': np.array([2,   2])}
 mesh.set_elements_volumes()
 # set data 
+n_bndf = np.size(mesh.bndfaces,0)
+#
 arr_tmp = np.zeros((n_elem,3))
 arr_tmp[:,0] = 1. 
 mesh.elements_data['velocity'] = velocity * arr_tmp
 mesh.elements_data['temp'] = np.zeros((n_elem,1))
-n_bndf = np.size(mesh.bndfaces,0)
 arr_tmp = np.zeros((n_bndf,3))
 arr_tmp[:,0] = 1. 
 mesh.bndfaces_data['velocity'] =   velocity * arr_tmp 
+#
+arr_tmp = np.ones((n_elem,1))
+mesh.elements_data['diffusion'] = diffusion_coeff * arr_tmp
+arr_tmp = np.ones((n_bndf,1))
+mesh.bndfaces_data['diffusion'] = diffusion_coeff * arr_tmp
 
 boundary_conditions = {'inlet' : {'type' : 'dirichlet',
                                   'value' : 3},
@@ -37,21 +43,21 @@ boundary_conditions = {'inlet' : {'type' : 'dirichlet',
                        'wall' : {'type' : 'neumann',
                                  'value' : np.array([0,0,0])}}
 
-conv_op = CentralDiffConvection(velocity_data= 'velocity',convected_data = 'temp')
-diff_op = OrthogonalDiffusion()
+conv_op = CentralDiffConvection(velocity_data= 'velocity')
+diff_op = OrthogonalDiffusion(diffusion_data= 'diffusion')
 tstepper = ForwardEulerScheme()
 #
 meshsize = mesh.elements_volumes**(1/3)
 velocity_array = mesh.elements_data['velocity']
-dt_diff = tstepper._calc_dt_diff(fourier,diffusion_coeff,1,meshsize)
+diffusion_array = mesh.elements_data['diffusion']
+dt_diff = tstepper._calc_dt_diff(fourier,diffusion_array,1,meshsize)
 dt_conv = tstepper._calc_dt_conv(cfl,velocity_array, meshsize)
 dt = np.min([dt_diff, dt_conv])
 tstepper.set_timestep(dt)
 #
 mat_c, rhs_c = conv_op(mesh,boundary_conditions)
 mat_d, rhs_d = diff_op(mesh, 
-                     boundary_conditions, 
-                     diffusion_coeff=diffusion_coeff)
+                     boundary_conditions)
 mat = mat_c + mat_d
 rhs = rhs_c + rhs_d
 static_solution = np.dot(np.linalg.pinv(mat),rhs)
