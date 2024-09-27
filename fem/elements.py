fix bug in stress computation

fem/elements.py
@@ -570,5 +570,5 @@ class Tet4Vector(FemConstructor) :
         symgrad_dbf_arr = self.calc_global_dbf_array_symgrad(elcentroid, inv_jacobian)
         flatten_disp_arr = disp_arr.flatten()
         strain_tensor_arr = np.dot(np.transpose(symgrad_dbf_arr),flatten_disp_arr)
-        stress_tensor_arr = np.dot(np.linalg.inv(centroid_state_matrix),strain_tensor_arr)
+        stress_tensor_arr = np.dot(centroid_state_matrix,strain_tensor_arr)
         return stress_tensor_arr

tests/test_fe_constructor_tet_vector.py
@@ -176,6 +176,7 @@ def test_stress_computation(tet_fixture):
                          [0,0,0],
                          [0,0,0]])
     tet_fixture.set_element(element_coords)
-    tet_fixture.calc_stress_tensor(disp_arr,state_arr)
-    assertion = False 
+    ret_arr = tet_fixture.calc_stress_tensor(disp_arr,state_arr)
+    expected_arr = np.array([0, 0, 0.1, 0, 0, 0])
+    assertion = np.all(ret_arr == expected_arr)
     assert assertion 